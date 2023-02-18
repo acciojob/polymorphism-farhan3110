6c0cae3ac7d113dd7b8d8cde9f734e125f520779
farhan3110 <mdfarhan3110@gmail.com>
2023-02-18 20:28:34 +0530

    Polhy

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,33 @@
 package com.driver;
 
 public class Main {
+     class Product {
+        public int product(int x, int y) {
+            return x * y;
+        }
 
+        // Task 4: Overloaded method with signature public int product(int x, int y, int z)
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        // Task 5: Overloaded method with signature public double product(double x, double y)
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+    public static void main(String[] args) {
+
+        Product p = new Main().new Product();
+
+
+        int result1 = p.product(2, 3);
+        System.out.println(result1); // Output: 6
+
+        int result2 = p.product(2, 3, 4);
+        System.out.println(result2); // Output: 24
+
+        double result3 = p.product(2.5, 3.5);
+        System.out.println(result3); // Output: 8.75
+    }
 }
